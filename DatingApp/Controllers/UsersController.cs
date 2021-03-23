7fcba1f6fc7e60111d5c863c6d8dcff60af79457
arbiasGashi <arbias.gashi@outlook.com>
2021-03-23 12:22:51 +0100

    Modified Program.cs to execute migrations automatically and to seed user data if they dont initially exist

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace DatingAppAPI.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -20,14 +21,12 @@ namespace DatingAppAPI.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        [Authorize]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
             return await _context.Users.FindAsync(id);
